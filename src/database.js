12465a3759dfc5c6948a62e0040c91e16eb931ff
Bill Brinkert <william.brinkert@akqa.com>
2019-04-29 09:18:55 -0700

    feat: add example of read-all from cloudant, plus updating a record; all for reference purposes only

--- src/database.js
+++ src/database.js
@@ -17,3 +17,30 @@ connectCloudantDB().then(db => {
       console.log(err);
   });
 });
+
+// Read from Cloudant without query filter
+connectCloudantDB().then(db => {
+  db.list({ include_docs: true }).then(body => {
+    body.rows.forEach((row, index) => {
+      // If there is no PDF data, don't try to save anything.
+      console.log(`row ${index}:\n ${JSON.stringify(row, null, 2)}`);
+      let thisDoc = row.doc;
+      // This section is a test of pushing updates of entirely new content
+      // onto the record.
+      thisDoc.posts = [{"title": "Test", "content": "Some content"}];
+      comment = {
+        "username": "Fred",
+        "text": "Really interesting stuff" 
+      };
+      thisDoc.posts[0].comments = [];
+      thisDoc.posts[0].comments.push(comment);
+      db.insert(thisDoc)
+        .then(data => {
+          console.log("updated: ", data);
+        })
+        .catch(err => {
+          console.log(err);
+      });
+    });
+  });
+});
